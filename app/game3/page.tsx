a9b42335d19498791372d734bf553a97088a3aa6
acabochan <166557187+acabochan@users.noreply.github.com>
2025-02-01 17:49:33 -0500

    UI updates

--- app/game3/page.tsx
+++ app/game3/page.tsx
@@ -1,3 +1,5 @@
+import '../styles/globals.css'; 
+
 export default function game3() {
   return (
     <div>Hello</div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,13 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
+
   theme: {
     extend: {
+      fontFamily: {
+        PixelifySans: ['"Pixelify Sans"', 'sans-serif'],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
